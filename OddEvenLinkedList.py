23824bf5fd1c41618e0d813e3ca407f6d8c7d9fa
yash mantri <mantriyash2025@gmail.com>
2020-05-16 19:47:05 +0530

    soln

---
+++ OddEvenLinkedList.py
@@ -0,0 +1,27 @@
+'''
+not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL
+Output: 1->3->5->2->4->NULL
+Example 2:
+
+Input: 2->1->3->5->6->4->7->NULL
+Output: 2->3->6->7->1->5->4->NULL'''
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        dummy1 = odd = ListNode(0)
+        dummy2 = even = ListNode(0)
+        print(dummy1,even)
+        while head:
+            odd.next = head
+            even.next = head.next
+            odd = odd.next
+            even = even.next
+            head = head.next.next if even else None
+        odd.next = dummy2.next
+        return dummy1.next
