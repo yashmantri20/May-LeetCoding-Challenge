ba4c358be6ab7c4de48a0ff01ddf1af9946470d5
yash mantri <mantriyash2025@gmail.com>
2020-05-18 15:44:01 +0530

    soln

---
+++ PermutationInString.py
@@ -0,0 +1,31 @@
+'''
+Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first string's permutations is the substring of the second string.
+
+Example 1:
+
+Input: s1 = "ab" s2 = "eidbaooo"
+Output: True
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input:s1= "ab" s2 = "eidboaoo"
+Output: False
+
+Note:
+
+The input strings only contain lower case letters.
+The length of both given strings is in range [1, 10,000].'''
+
+
+
+class Solution:
+	def checkInclusion(self, s: str, t: str) -> bool:
+		LS, LT, S, T = len(s), len(t), 0, 0
+		if LS > LT: return False     
+		for i in range(LS): S, T = S + hash(s[i]), T + hash(t[i])
+		if S == T: return True
+		for i in range(LS, LT):
+			T += hash(t[i]) - hash(t[i-LS])
+			if S == T: return True
+		return False
+		
